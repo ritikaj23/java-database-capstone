9d88c6d1d61b9e9969c7dbcd76690de42488ba41
ritikaj23 <ritikaj@skillup.online>
2025-05-27 05:08:10 -0400

    modified requirements.txt

---
+++ back-end/test/java/com/example/java_database_capstone/JavaDatabaseCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_database_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDatabaseCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
